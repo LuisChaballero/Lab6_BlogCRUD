7549be19238197581d7b69b32416250151268867
Luis Caballero <luis_cn98@hotmail.com>
2020-01-24 23:26:40 -0600

    Added functionality to search and eliminate comment

--- public/js/index.js
+++ public/js/index.js
@@ -28,8 +28,8 @@ function showComments(){
             });
 
         },
-        error: function(err){
-            console.log(err);
+        error: function(error){
+            console.log(error);
         }
     });
 }
@@ -63,15 +63,13 @@ function addComment(){
 
 function modifyComment(){
     $('#commentModification').on('submit', function(e){
-
         e.preventDefault();
 
         let id = $('#idMod').val();
         let titulo = $('#tituloMod').val();
         let contenido = $('#contenidoMod').val();
         let autor = $('#autorMod').val();
-        
-        console.log("hola2");
+
         $.ajax({
             type: "PUT",
             url: `/blog-api/actualizar-comentario/`+id,
@@ -83,20 +81,94 @@ function modifyComment(){
               location.reload();
             },
             error: function ({status, errorMessage}) {
-                console.log("err");
                 alert(`Error ${status}: ${errorMessage}`);
             }
           
         });
-        console.log("fin");
 
     });
 } 
+
+function deleteComment() {
+    $('#commentDelete').on('submit', function (e) {
+        let id = $('#idElim').val();
+
+        $.ajax({
+            type: "DELETE",
+            url: "/blog-api/remover-comentario/" + id,
+            success: function (response) {
+                location.reload();
+            },
+            error: function ({
+                status,
+                errorMessage
+            }) {
+                alert(`Error ${status}: ${errorMessage}`);
+            }
+        });
+    });
+}
+
+function searchComments(){
+    $('#commentSearch').on('submit', function(e){
+        console.log("hol");
+        e.preventDefault();
+        let autor = $('#autorSearch').val();
+
+        $.ajax({
+            method: "GET",
+            url: "/blog-api/comentarios-por-autor?autor="+autor,
+            dataType: "json",
+            success: response =>{    
+                console.log("suc");
+                //$('#author').empty();
+                $('#commentAutor').empty();
+/*
+                $("#author").append(`
+                        <h2>${autor}</h2>
+                `);
+*/
+                response.forEach(comentario => {
+                    $("#commentAutor").append(`
+                        <section class="commentFormat">
+                            <div class="commentHeader">
+                                <h2>${comentario.autor}</h2>
+                            </div>
+                            <div class="commentTitle">
+                                <h3>${comentario.titulo}</h3>
+                            </div>
+                            <div class="commentContent">
+                                <p>${comentario.contenido}</p>
+                            </div>
+                            <div class="commentDate">
+                                <p>${comentario.fecha}</p>
+                            </div>
+                            <div class="commentFooter">
+                                <p>${comentario.id}</p>
+                            </div>
+                        </section>                
+                    `);
+                });
+    
+            },
+            error: function ({status, errorMessage}) {
+                console.log("err");
+                alert(`Error ${status}: ${errorMessage}`);
+            }
+        });
+
+    });
+
+    
+}
+
 
 function init(){
     showComments();
     addComment();
     modifyComment();
+    deleteComment();
+    searchComments();
 }
 
 init();
